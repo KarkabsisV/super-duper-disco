b4c6ee128988d77813326c552741dc21616bda2b
KarkabsisV <vitalii.kulinskyi@gmail.com>
2020-10-12 19:46:15 +0300

    Query

---
+++ script/jq.js
@@ -0,0 +1,15 @@
+// $(function(){
+//     alert('Hello Jq!');
+// });
+
+// $(function() {
+//     $('#hello').click(function(){
+//         $("i").hide();
+//     })
+
+// });
+
+$('p').on({
+    'click': function() {console.log('clicked!!')},
+    'mouseover': function() {console.log('Hovered!!!!');}
+ });
